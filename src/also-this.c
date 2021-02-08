7d58a0da6801bd81e8807f82c68cdcf631b987c6
Michael Paulson <mpaulson@netflix.com>
2021-02-08 14:45:41 -0800

    feat(more-files)

---
+++ src/also-this.c
@@ -0,0 +1,7 @@
+
+// yes one more
+//
+int main() {
+    for (int i = 0; i < SOCKET_OPEN; ++i) {
+    }
+}
---
+++ src/another.c
@@ -0,0 +1,7 @@
+
+int another_function() {
+    int i = -1;
+    do {
+        ++i;
+    } while (i == SOCKET_OPEN);
+}
